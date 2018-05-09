287340815851434dab2ccae75ec60f7d24441fce
Azucena <a.ortizp@alumnos.upm.es>
2018-05-10 01:10:39 +0200

    P5 core

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+const {models} =require("../models/index");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/credits', function (req, res, next) {
+   res.render('credits', {title: 'Credits'});
+});
+
+router.get('/quizzes', function (req, res, next) {
+    
+    models.quiz.findAll()
+    .then(quizzes => {
+		 res.render('quizzes/index', {quizzes});
+	})
+});
+
+module.exports = router;
